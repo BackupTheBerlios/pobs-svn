fca1fca19a8fd39bdc03574b72c16471a3abcec7
rajesh <rajesh@3803574f-21e2-0310-9265-babcdfdb5765>
2004-08-21 07:36:59 +0000

    Cleaning up the repos and add necesary files.

---
+++ trunk/cmd_line/pobs.php
@@ -0,0 +1,32 @@
+#!/bin/php -q
+
+<?php
+
+/*
+   +--------------------------------------------------------------------+
+   | POBS - The PHP Obfuscator                                          |
+   +--------------------------------------------------------------------+
+   | Copyright (c) 2004 RKS Development Labs                            |
+   +--------------------------------------------------------------------+
+   | This program is free software; you can redistribute it and/or      |
+   | modify it under the terms of the GNU General Public License as     |
+   | published by the Free Software Foundation; either version 2 of the |
+   | License, or (at your option) any later version.                    |
+   |                                                                    |
+   | This program is distributed in the hope that it will be useful,    |
+   | but WITHOUT ANY WARRANTY; without even the implied warranty of     |
+   | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the file  |
+   | COPYING included with this source for more details.                |
+   +--------------------------------------------------------------------+
+   | August 20 2004                                                     |
+   | Version 1.00                                                       |
+   +--------------------------------------------------------------------+
+   | AUTHOR: Rajesh Kumar <rajesh@meetrajesh.com>                       |
+   +--------------------------------------------------------------------+
+  
+   $Id$
+
+*/
+
+
+?>
